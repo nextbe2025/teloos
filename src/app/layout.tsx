2bdb93ad2866cff0220b0bf656ca443dfd53785f
eurodrigobueno <rodrigo@updo.com.br>
2026-06-04 21:12:17 -0300

    Revert "fix: recarrega automaticamente em ChunkLoadError após deploy"
    This reverts commit cd1d8c6c972f2b9681ec88605ce0fcb76f7e7bfe.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 import { ScrollToTop } from '@/components/ui/scroll-to-top'
 import { GTMProvider } from '@/components/shared/gtm-provider'
-import { ChunkErrorReload } from '@/components/shared/chunk-error-reload'
 import { siteConfig } from '@/config/site'
 import { GTM_ID } from '@/lib/gtm'
 import './globals.css'
@@ -94,8 +93,6 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-
-        <ChunkErrorReload />
 
         <ThemeProvider
           attribute="class"
